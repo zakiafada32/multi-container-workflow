33cb6bac6f9827b8bf43a377128200da27f2127f
zakiafada32 <afadazaki32@gmail.com>
2021-04-20 23:09:10 +0700

    add react as client and fib component

---
+++ client/src/Fib.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Fib: React.FC = () => {
+  const [seenIndexes, setSeenIndexes] = useState<any[]>([]); //todo update type
+  const [values, setValues] = useState<any>({}); // todo update type
+  const [index, setIndex] = useState<string>('');
+
+  useEffect(() => {
+    fetchValues();
+    fetchIndexes();
+  }, []);
+
+  const fetchValues = async () => {
+    const values = await axios.get('/api/values/current');
+    setValues(values.data);
+  };
+
+  const fetchIndexes = async () => {
+    const seenIndexes = await axios.get('/api/values/all');
+    setSeenIndexes(seenIndexes.data);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await axios.post('/api/values', {
+      index: index,
+    });
+    setIndex('');
+  };
+
+  const renderSeenIndexes = () => {
+    return seenIndexes.map(({ number }) => number).join(', ');
+  };
+
+  const renderValues = () => {
+    const entries = [];
+
+    for (let key in values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {values[key]}
+        </div>
+      );
+    }
+
+    return entries;
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your index</label>
+        <input
+          value={index}
+          onChange={(event) => setValues(event.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+
+      <h3>Indexes I have seen:</h3>
+      {renderSeenIndexes()}
+
+      <h3>Calculated Values:</h3>
+      {renderValues()}
+    </div>
+  );
+};
+
+export default Fib;
---
+++ client/src/OtherPage.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const OtherPage = () => {
+  return (
+    <div>
+      Other page
+      <Link to="/">Back to home</Link>
+    </div>
+  );
+};
+
+export default OtherPage;
